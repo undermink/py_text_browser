99e66f97648e01916f8237c0146bfadc482f2c7d
undermink <undermink@devtal.de>
2013-04-06 03:18:07 +0200

    init

---
+++ browser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib
+
+body = ""
+print "\nWillkommen...\n"
+InputUrl = raw_input('\nBitte eine URL eingeben: ')
+url = InputUrl.strip("http://")
+Response = urllib.urlopen("http://"+url)
+bs = BeautifulSoup(Response.read(), "lxml")
+if Response.code != 200 :
+	print "Fehler :",Response.code
+else :
+	print bs.title.text
+	html = bs.body.text.strip()
+	for line in html.split("\n") :
+		if line.strip() :
+			body += line + "\n"
+	print body
+
+
