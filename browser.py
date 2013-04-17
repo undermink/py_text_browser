3a4788b4fe7a7dd72ddddb923c6ee84d5a7fd0d1
undermink <undermink@devtal.de>
2013-04-17 23:42:59 +0200

    show links

--- browser.py
+++ browser.py
@@ -4,19 +4,38 @@ from bs4 import BeautifulSoup
 import urllib
 
 body = ""
+count= 0
+
 print "\nWillkommen...\n"
+
 InputUrl = raw_input('\nBitte eine URL eingeben: ')
+
 url = InputUrl.strip("http://")
+
 Response = urllib.urlopen("http://"+url)
 bs = BeautifulSoup(Response.read(), "lxml")
+
 if Response.code != 200 :
+
 	print "Fehler :",Response.code
+
 else :
+
 	print bs.title.text
 	html = bs.body.text.strip()
-	for line in html.split("\n") :
-		if line.strip() :
-			body += line + "\n"
-	print body
 
+	for line in html.split("\n") :
+
+		if line.strip() :
+
+			body += line + "\n"
+
+	print body
+	
+	print "\nLinks:\n"
+	for link in bs.find_all('a') :
+
+		if link.get('href') != None :
+			count += 1
+			print link.get('href') , [count]
 
